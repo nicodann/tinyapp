dd81a5fa0927ab1d95ef19ddd4327953718b04c0
Nico Dann <nicodann@gmail.com>
2021-11-29 15:40:15 +0000

    Make route for single URLs, use req.param, create header

--- express_server.js
+++ express_server.js
@@ -19,9 +19,14 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 })
 
-app.get("/urls.json", (req,res) => {
-  res.json(urlDatabase);
-});
+app.get("/urls/:shortURL", (req,res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render("urls_show", templateVars);
+})
+
+// app.get("/urls.json", (req,res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/hello", (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
