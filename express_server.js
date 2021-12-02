01f7acc17f7607c0c7d5ac8bbdf08789b502273c
Nico Dann <nicodann@gmail.com>
2021-12-02 21:29:17 +0000

    Display username from cookie in header

--- express_server.js
+++ express_server.js
@@ -12,6 +12,9 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 const generateRandomString = (length) => {
   let ranString = "";
   const alphNumChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,25 +26,29 @@ const generateRandomString = (length) => {
 
 //////////// ROUTING BELOW
 
-app.get("/", (req,res) => {
+app.get("/", (_req,res) => {
   res.send("Hello!");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("urls");
 });
 
 app.get("/urls", (req,res) => {
   const templateVars = {
     username: req.cookies["username"],
-    urls:urlDatabase 
+    urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect("urls")
-});
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -53,7 +60,11 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req,res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  }
   res.render("urls_show", templateVars);
 })
 
