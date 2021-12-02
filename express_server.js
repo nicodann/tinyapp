d3f884381c9c87fc2cf49f4802cce986cebb5776
Nico Dann <nicodann@gmail.com>
2021-12-02 14:19:46 +0000

    Make delete buttons for URLs.  Add appropriate routing.

--- express_server.js
+++ express_server.js
@@ -54,6 +54,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req,res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
+
 app.get("/hello", (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
